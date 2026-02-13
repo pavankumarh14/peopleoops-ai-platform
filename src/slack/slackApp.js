2849f23eb332bcb49c75ad711c956e266a159673
pavankumarh14 <pavanh14@gmail.com>
2026-02-14 02:02:37 +0530

    Create slackApp.js

---
+++ src/slack/slackApp.js
@@ -0,0 +1,9 @@
+// Slack App configuration
+const { App } = require('@slack/bolt');
+
+const slackApp = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+module.exports = slackApp;
