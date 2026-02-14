2ebef7d3a6bbf6105bdb10f6db2bcad9d075bd36
pavankumarh14 <pavanh14@gmail.com>
2026-02-14 08:25:40 +0530

    Update router.js

--- src/agents/router.js
+++ src/agents/router.js
@@ -1,4 +1,29 @@
-// Agent router
-module.exports = {
-  // Route agent requests
-};
+const hrAgent = require("./hrAgent");
+const performanceAgent = require("./performanceAgent");
+const compensationAgent = require("./compensationAgent");
+
+async function route(intent, entities) {
+
+  switch (intent) {
+
+    case "hike_simulation":
+      return await compensationAgent.simulateHike(
+        entities.percentage
+      );
+
+    case "performance_summary":
+      return await performanceAgent.getPerformanceSummary(
+        entities.employee_id
+      );
+
+    case "employee_lookup":
+      return await hrAgent.getEmployeeProfile(
+        entities.employee_id
+      );
+
+    default:
+      return "Intent not supported.";
+  }
+}
+
+module.exports = route;
