dde9474e05d1fdf4d7278df80c36791df010f377
pavankumarh14 <pavanh14@gmail.com>
2026-02-14 08:23:48 +0530

    Update queries.js

--- src/esql/queries.js
+++ src/esql/queries.js
@@ -1,4 +1,19 @@
-// ES|QL Queries
 module.exports = {
-  // Query definitions
+
+  employeeProfile: (id) => `
+    FROM hr_employees
+    | WHERE employee_id == "${id}"
+  `,
+
+  performanceAverage: (id) => `
+    FROM hr_performance
+    | WHERE employee_id == "${id}"
+    | STATS avg_rating = AVG(rating)
+  `,
+
+  salaryBudgetSimulation: (percentage) => `
+    FROM hr_employees
+    | STATS total_salary = SUM(salary)
+    | EVAL hike_budget = total_salary * ${percentage / 100}
+  `
 };
