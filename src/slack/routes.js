1b747057bfe2a58a4329b307d6ea9aebd714621b
pavankumarh14 <pavanh14@gmail.com>
2026-02-14 10:59:13 +0530

    Update routes.js

--- src/slack/routes.js
+++ src/slack/routes.js
@@ -1,4 +1,7 @@
-// Slack routes configuration
-module.exports = function(app) {
-  // Define routes here
-};
+const express = require("express");
+const router = express.Router();
+const handleSlackEvent = require("./slackApp");
+
+router.post("/events", handleSlackEvent);
+
+module.exports = router;
